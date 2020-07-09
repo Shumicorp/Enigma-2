c2ac07463b06d15da36b12c1c589ee610d6efd7a
Mikle <Shumicorp@gmail.com>
2020-07-09 12:35:57 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Enigma_2
+{
+    partial class Enigma
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(574, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 250);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 178);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(524, 102);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Send (Відправити)",
+            "Take (Отримати)"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(244, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ComboBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(12, 326);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(524, 102);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Joise doing";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Code Text";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(498, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox3.Size = new System.Drawing.Size(206, 26);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox3_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(444, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Name ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 29);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(244, 27);
+            this.textBox4.TabIndex = 10;
+            // 
+            // Enigma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(737, 458);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Enigma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enigma_by_Shumicorp 2.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Enigma_2.Properties;
+namespace Enigma_2
+{
+    public partial class Enigma : Form
+    {
+        public Enigma()
+        {
+            InitializeComponent();
+            textBox3.Text = Settings.Default["TextName"].ToString();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }   
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int l = 0;
+            int k = 0;
+
+            string code = Convert.ToString(textBox3.Text);
+            textBox2.Text = null;
+            string t = textBox1.Text;
+            string w = null;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: w = "send"; break;
+                case 1: w = "take"; break;
+            }
+            
+            if (w == "send")
+            {
+                for (int i = 0; i < t.Length; i++)
+                {
+                    if ((t[i] > 64 && t[i] < 91) || (t[i] > 96 && t[i] < 123))
+                    {
+                        if (char.IsLower(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] + k) - 97) % 26) + 97));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                        else if (char.IsUpper(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] + k) - 65) % 26) + 65));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                    }
+                    else if (t[i] > 1039 && t[i] < 1104)
+                    {
+                        if (char.IsLower(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] + k) - 1072) % 32) + 1072));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                        else if (char.IsUpper(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] + k) - 1040) % 32) + 1040));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                        
+                    }
+                    else
+                        textBox2.Text += Convert.ToString(t[i]);
+                } 
+            }
+            if (w == "take")
+            {
+                for (int i = 0; i < t.Length; i++)
+                {
+                    if ((t[i] > 64 && t[i] < 91)|| (t[i] > 96 && t[i] < 123))
+                    {
+                        if (char.IsLower(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] - k) - 122) % 26) + 122));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                        else if (char.IsUpper(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] - k) - 90) % 26) + 90));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                        
+                    }
+                    else if (t[i] > 1039 && t[i] < 1104)
+                    {
+                        if (char.IsLower(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] - k) - 1103) % 32) + 1103));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                        else if (char.IsUpper(t[i]))
+                        {
+                            k = swift(code[l]);
+                            textBox2.Text += Convert.ToString(Convert.ToChar((((t[i] - k) - 1071) % 32) + 1071));
+                            l++;
+                            if (l >= code.Length)
+                            { l = 0; }
+                        }
+                    }
+                    else
+                        textBox2.Text += Convert.ToString(t[i]);
+                                                        
+
+                }
+            }
+            //*/
+            Settings.Default["Textname"] = textBox3.Text;
+            Settings.Default.Save();
+        }
+
+        public static int swift(char c)
+        {
+            int k = 0;
+            if (char.IsLower(c))
+            { k = c - 97; }
+            else
+            { k = c - 65; }
+            return k;
+        }
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void TextBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 'a') && (e.KeyChar <= 'z') || (e.KeyChar >='A') && (e.KeyChar <='Z'))
+                return;
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    if (sender.Equals(textBox1))
+                        textBox2.Focus();
+                    else
+                        return;
+                }
+                return;   
+            }
+            e.Handled = true;
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void ComboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
